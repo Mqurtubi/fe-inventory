32c236c59a20571457889bff0f1a2530eeff142b
qurtubi <qurtubi1411@gmail.com>
2026-01-18 20:35:11 +0700

    update

--- src/app/router/ProtectedRouter.tsx
+++ src/app/router/ProtectedRouter.tsx
@@ -2,8 +2,8 @@ import { Navigate } from "react-router-dom";
 import { useAppSelector } from "../../hooks/redux";
 import type { ProtectedType } from "./type";
 export function ProtectedRoute({ children, roles }: ProtectedType) {
-  const { user } = useAppSelector((s) => s.auth);
-
+  const { user, loading } = useAppSelector((s) => s.auth);
+  if (loading) return;
   if (!user) return <Navigate to="/login" replace />;
 
   if (roles && !roles.includes(user.role)) {
